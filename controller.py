05d2638c4bd05f43063436a5e77d8d46721c6dbf
aswarbs <amberswarbrick1@gmail.com>
2024-06-01 11:35:27 +0100

    initial client server

---
+++ controller.py
@@ -0,0 +1,90 @@
+    # JazzHands | JazzHandsController
+# Written by Amber Swarbrick 06/01/2024
+# Code Review Passed by Toby Clark 06/01/2024
+
+from gamemaker_handler import GMS2Client
+from pi_handler import PiClient
+from queue import Queue
+
+class JazzhandsController():
+    running: bool                                   # A flag to determine if the program has ended. Facilitates safe termination of threads.
+    gamemaker_client: GMS2Client 
+    pi_client: PiClient
+    gamemaker_queue: Queue                             # An instance of the client queue. Facilitates sending gesture data to the client object.
+    gesture_queue: Queue                            # An instance of the gesture_recognition queue. Allows transmission of gesture data to the client.
+
+    def __init__(self) -> None:
+        """
+        Main function which initiates the client and gesture recognition.
+        """
+
+        self.create_client()
+        self.create_pi_client()
+
+        # Boolean flag to continuously run the controller until a stopping condition is met.
+        self.running = True
+        
+    def create_client(self) -> None:
+        """
+        Creates a thread which initialises the client.
+        """
+
+        self.gamemaker_client = GMS2Client()
+        self.gamemaker_queue: Queue = self.gamemaker_client.client_queue
+        self.gamemaker_client.start_thread()
+
+
+    def create_pi_client(self) -> None:
+        """
+        Creates a thread which initialises the pi client.
+        """
+        self.pi_client = PiClient()
+        self.pi_queue: Queue = self.pi_client.client_queue
+        self.pi_client.start_thread()
+
+    def mainloop(self) -> None:
+        """
+        Program Main Loop. Retrieves gesture detections and sends them to GameMaker server.
+        """
+        while self.running:
+            self.update_client()
+        print("stopped running")
+
+    def update_client(self) -> None:
+        """
+        Attempt to update the client. Handle keyboard interrupt exceptions.
+        """
+        try:
+            self.try_transmit_to_client()
+
+        # The program will safely end upon a keyboard interrupt (Ctrl+C).
+        except KeyboardInterrupt:
+            self.terminate_program()
+        except Exception as e:
+            raise e
+    
+    def terminate_program(self) -> None:
+        """
+        Safely end all threads and terminate the main process.
+        """
+        print("CTRL+C has been pressed. Ending threads.")
+        self.pi_client.stop_thread()
+        self.gamemaker_client.stop_thread()
+        self.running = False
+        exit(0)
+
+    def try_transmit_to_client(self) -> bool:
+        """
+        Attempt to send the most recent gesture from gesture_queue to the GMS2 server.
+        """
+        if not self.gesture_queue.empty():
+            gestures: str = self.gesture_queue.get()
+            print(gestures)
+            self.gamemaker_queue.put(gestures)
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    JazzhandsController()
---
+++ pi_handler.py
@@ -0,0 +1,183 @@
+
+
+import socket
+import threading
+from queue import Queue
+import json
+import base64
+import cv2
+from PIL import Image
+import io
+import numpy as np
+
+
+HOST = "192.168.195.65"
+PORT = 1234
+
+class PiClient():
+    stop_event: threading.Event     # Event to signal the termination of the thread.
+    client_queue: Queue             # Queue to transfer data from the subthread to the main thread.
+    thread: threading.Thread        # Client thread to maintain client-server connection.
+
+    def __init__(self):
+        """
+        Initializes stop event & client queue
+        """
+        # Create an event to signal the subthreads to safely stop execution.
+        self.stop_event = threading.Event()
+
+        self.client_queue = Queue()
+
+    def start_thread(self) -> None:
+        """
+        Initialise & Start the Client thread.
+        """
+
+        self.thread = self.create_thread()
+        self.thread.start()
+
+    def create_thread(self) -> threading.Thread:
+        """
+        Create the Client thread.
+        Returns: the client thread instance.
+        """
+
+        # ',' used in thread args to convert the single argument to a tuple.
+        pi_thread: threading.Thread
+        pi_thread = threading.Thread(
+            target=self.handle_connection_tcp, args=(self.stop_event,)
+        )
+        return pi_thread
+    
+    def stop_thread(self) -> None:
+        """
+        Stop the client thread using the stop_event.
+        """
+
+        self.stop_event.set()
+        self.thread.join()
+
+    def handle_connection_tcp(self, stop_event:threading.Event) -> bool:
+        """
+        Initialise the connection to the server.
+        Handle message transmission between the client and server.
+
+        Args:
+            client_queue: A Queue shared between the client thread and controller. Stores messages to be sent from the client to the server.
+            stop_event: A threading.Event instance that will be set once the thread should terminate.
+        """
+
+        # Initialise the socket and bind it to the specified host, at the specified port.
+        sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.bind((HOST, PORT))
+        print("listening")
+        sock.listen()
+
+        # Attempt connection to the server.
+        try:
+            conn: socket.socket
+            conn, _ = sock.accept()
+            print("connected pi")
+
+        # Handle socket timeout
+        except socket.timeout:
+            print("Failed to connect to GMS2 due to timeout.\nTry increasing timeout length in settings.ini")
+            return False
+        except Exception as e:
+            raise e
+
+        with conn:  
+            self.mainloop(stop_event, conn)
+
+    def mainloop(self, stop_event: threading.Event, conn:socket.socket) -> None:
+        """
+        Send messages to the GMS2 server if any are queued.
+        """
+
+        # if the pi has sent an image (pi queue not empty), process it
+        while not stop_event.is_set():
+
+            
+            received_data = self.receive_data(conn)
+            received_data = json.loads(received_data)
+
+            if received_data["has_image"]:
+
+
+                parsed_data = self.parse_data(received_data)
+
+                image = self.convert_bytes_to_image(parsed_data)
+
+                cv2.imshow("", image)
+                cv2.waitKey(1)
+
+            else:
+
+                print("sending message!")
+                self.send_response(conn)
+
+
+
+
+        return None
+
+
+    def receive_data(self, conn):
+        # Initialize an empty byte string to accumulate data
+        received_data = b""  
+
+        while True:
+            # Receive 1024 bytes of data
+            data = conn.recv(1024)
+
+            # Append the newly received data to the current item of data being collected
+            received_data += data  
+
+            # If the message delimiter is in the message, the end of the message has been found
+            if b'\n' in data:
+                break
+
+        return received_data
+
+    def parse_data(self, received_data):
+        try:
+            # Attempt to parse the message with JSON. Agreed encoding = UTF8
+            parsed_data = json.loads(received_data.decode('utf-8'))
+            return parsed_data
+        except:
+            pass
+
+
+    def convert_bytes_to_image(self, parsed_data):
+        # Retrieve the base64-encoded image data
+        base64_image_data = parsed_data['screenshotPNG']
+
+        # Decode the base64 string to bytes
+        image_data_bytes = base64.b64decode(base64_image_data)
+
+        # Create a BytesIO object from the decoded bytes
+        image_stream = io.BytesIO(image_data_bytes)
+
+        # Open the image using PIL (Pillow)
+        image = Image.open(image_stream)
+
+        # Convert the PIL Image to a NumPy array
+        image_array = np.array(image)
+
+        # Convert RGB to BGR (this step is necessary only if your image is in color)
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
+
+        cv2.imshow("", image_array)
+        cv2.waitKey(1)
+
+        image_np = np.asarray(image)
+
+        return image_np
+            
+    
+    def send_response(self, conn):
+
+            success_response = {"take_photo": True}
+            
+            conn.send(json.dumps(success_response).encode('utf-8') + b'\n')
